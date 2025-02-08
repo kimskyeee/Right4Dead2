65f031741dc1284d739a4ef01b32ef88e6df7cb2
byungmeo_wanted <wlsrlfvkr@gmail.com>
2025-02-09 07:30:13 +0900

    Add log macro

--- Source/Right4Dead/Right4Dead.h
+++ Source/Right4Dead/Right4Dead.h
@@ -4,3 +4,11 @@
 
 #include "CoreMinimal.h"
 
+#define CALLINFO (FString(__FUNCTION__) + TEXT("(") + FString::FromInt(__LINE__) +TEXT(")"))
+#define PRINT_CALLINFO() \
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *CALLINFO)
+#define PRINTLOG(fmt, ...) \
+	UE_LOG(LogTemp, Warning, TEXT("%s %s"), *CALLINFO, *FString::Printf(fmt, ##__VA_ARGS__))
+#define PRINTLOGTOSCREEN(fmt, ...) \
+	PRINTLOG(fmt, ##__VA_ARGS__);\
+	GEngine->AddOnScreenDebugMessage(0, 1, FColor::Cyan, *FString::Printf(fmt, ##__VA_ARGS__))
