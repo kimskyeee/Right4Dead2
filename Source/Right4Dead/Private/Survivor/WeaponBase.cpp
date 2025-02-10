ed8c6bba43ebc02eaa07210a384ab0aaeb6ede69
kimskyeee <zlagksnf@gmail.com>
2025-02-10 14:25:00 +0900

    0210 input

---
+++ Source/Right4Dead/Private/Survivor/WeaponBase.cpp
@@ -0,0 +1,96 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WeaponBase.h"
+
+#include "Survivor.h"
+#include "Camera/CameraComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
+
+
+// Sets default values
+AWeaponBase::AWeaponBase()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//외관
+	PrimaryWeapon=CreateDefaultSubobject<USkeletalMeshComponent>("PrimaryWeapon");
+	Muzzle=CreateDefaultSubobject<USceneComponent>("Muzzle");
+	Muzzle->SetupAttachment(PrimaryWeapon);
+	Muzzle->SetRelativeLocationAndRotation(FVector(0,57,-2.5),FRotator(0,90,0));
+	Eject=CreateDefaultSubobject<USceneComponent>("Eject");
+	Eject->SetupAttachment(Muzzle);
+	Eject->SetRelativeLocation(FVector(-50,0,0));
+	
+}
+
+// Called when the game starts or when spawned
+void AWeaponBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AWeaponBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+
+//공격함수 추가
+void AWeaponBase::LineTrace(FVector MuzzleLocation, FVector ImpactPoint,FRotator ProjectileRotation)
+{
+	ASurvivor* target = Cast<ASurvivor>(UGameplayStatics::GetPlayerCharacter(GetWorld(),0));
+	
+	FHitResult HitResult;
+	FVector SpreadStart;
+	FVector CameraFVector;
+	FCollisionQueryParams Params;
+	//나를 무시하는 조건을 추가해주기
+	Params.AddIgnoredActor(this);
+	
+	if (target)
+	{
+		SpreadStart = target->FirstCameraComp->GetComponentLocation();
+		CameraFVector = target->FirstCameraComp->GetForwardVector();
+	}
+
+	float End = UKismetMathLibrary::RandomFloatInRange(BulletSpread*(-1),BulletSpread);
+	FVector SpreadEnd = (CameraFVector*20000) + FVector(End,End,End);
+	
+	GetWorld()->LineTraceSingleByChannel(HitResult,SpreadStart,SpreadEnd,ECC_Visibility,Params);
+
+	FVector MuzzleEnd = HitResult.ImpactPoint;
+	FVector MuzzleStart = Muzzle->GetComponentLocation();
+
+	GetWorld()->LineTraceSingleByChannel(HitResult,MuzzleStart,MuzzleEnd,ECC_Visibility,Params);
+
+	MuzzleLocation = MuzzleStart;
+	ImpactPoint = HitResult.ImpactPoint;
+	ProjectileRotation = UKismetMathLibrary::FindLookAtRotation(HitResult.TraceStart, HitResult.TraceEnd);
+
+}
+
+
+void AWeaponBase::HideAmmo()
+{
+	if (PrimaryWeapon) //null guard
+	{
+		PrimaryWeapon->USkinnedMeshComponent::HideBoneByName("bullet",PBO_None);
+		PrimaryWeapon->USkinnedMeshComponent::HideBoneByName("bullets",PBO_None);
+	}
+}
+
+void AWeaponBase::UnHideAmmo()
+{
+	if (PrimaryWeapon)
+	{
+		PrimaryWeapon->USkinnedMeshComponent::UnHideBoneByName("bullet");
+		PrimaryWeapon->USkinnedMeshComponent::UnHideBoneByName("bullets");
+	}
+}
+
+
---
+++ Source/Right4Dead/Public/Survivor/WeaponBase.h
@@ -0,0 +1,74 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EWeapon.h"
+#include "GameFramework/Actor.h"
+#include "WeaponBase.generated.h"
+
+UCLASS()
+class RIGHT4DEAD_API AWeaponBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AWeaponBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	//외관
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	class USkeletalMeshComponent* PrimaryWeapon;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	class USceneComponent* Muzzle;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	class USceneComponent* Eject;
+
+	//함수 생성
+	void LineTrace(FVector MuzzleLocation, FVector ImpactPoint,FRotator ProjectileRotation);
+	UFUNCTION(BlueprintCallable) //BP에서 구현하지 않고 cpp에서 구현한거 사용만 할때
+	void HideAmmo();
+	UFUNCTION(BlueprintCallable)
+	void UnHideAmmo();
+
+	//Event
+	UFUNCTION(BlueprintImplementableEvent,Category="Event") //BP에서 구현할 때
+	void Fire();
+	UFUNCTION(BlueprintImplementableEvent,Category="Event")
+	void Reload();
+
+	//Recoil (반동) 변수
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Recoil")
+	FRotator ControllerRot;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Recoil")
+	FRotator PostRecoilRot;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Recoil")
+	FRotator MaxRecoilRot;
+
+	//Weapon 변수
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Weapon")
+	float ReloadTime;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Weapon")
+	float BulletSpread;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Weapon")
+	float AmmoCount;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Weapon")
+	float MaxAmmo;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Weapon")
+	EWeapon Name;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Weapon")
+	bool bWeaponMode;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Weapon")
+	bool bVariableMode;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Weapon")
+	bool bISShotgun;
+	
+};
