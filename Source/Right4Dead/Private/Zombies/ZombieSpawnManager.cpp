72a7693e50dd093798b2ac67a3558940db48e355
byungmeo_wanted <wlsrlfvkr@gmail.com>
2025-02-11 22:32:59 +0900

    Create Spawner C++ Class

---
+++ Source/Right4Dead/Private/Zombies/ZombieSpawnManager.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ZombieSpawnManager.h"
+
+
+// Sets default values
+AZombieSpawnManager::AZombieSpawnManager()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AZombieSpawnManager::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AZombieSpawnManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/Right4Dead/Private/Zombies/ZombieSpawnPoint.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ZombieSpawnPoint.h"
+
+
+// Sets default values
+AZombieSpawnPoint::AZombieSpawnPoint()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AZombieSpawnPoint::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AZombieSpawnPoint::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
